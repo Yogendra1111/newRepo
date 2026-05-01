ed617a9e72d618f7dd0a6d5a9fbe77232abc3b80
yogendra1111 <yogendranagar222005@gmail.com>
2026-05-01 13:29:56 +0530

    intial comment

---
+++ first.js
@@ -0,0 +1,209 @@
+//console.log("Welcome Mr.Yogendra");
+
+
+
+//1. Arithmetic operators
+//Uinary operator
+//post & pre 
+
+// let a = 5;
+
+// console.log("a = ",a);
+// console.log("a++ = ",a++);
+// console.log("a = ",a++);
+
+// console.log("a = ",a);
+// console.log("a-- = ",a--);
+// console.log("a = ",a--);
+
+
+
+//2. Asignment operator
+
+// console.log("current a value = ",a);
+// a += 5;
+// console.log("after operation a = ",a);
+
+//multiplication operation
+// let x = 10;
+// console.log("current value of x =", x);
+// x *= 5;
+// console.log("after operation  the value of x = ", x);
+
+
+
+//comparision operatior
+
+//  !=
+// let a = 10;
+// let b = 20;
+
+// console.log("10 != 20", a!=b);//result = true
+
+// //  ==
+// let c = 2;
+// let d = 2;
+
+// console.log("2==2", c==d);//result = true
+
+// //  ===
+// let p = 1;
+// let g = "4";
+
+// console.log("p===g", p===g );
+
+// //  !==
+// let h=4;
+// let y = 100;
+
+// console.log("h!==y",h!==y);
+
+// //   <,>,<=,>=;
+
+// let i = 5;
+// let o= 3;
+
+// console.log(i>o);
+// console.log(i<o);
+// console.log(i>=o);
+// console.log(i<=o);
+
+
+
+//operators
+
+//1. Arithmatic opeators
+
+// let a = 10;
+// let b = 20;
+
+//  console.log("a + b = ", a + b);
+// console.log("a - b = ", a - b);
+// console.log("a / b = ", a / b);
+// console.log("a * b = ", a * b);
+// console.log("a % b = ", a % b);
+// console.log("a ** b = ", a ** b);
+
+//unary operator
+//post & pre
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ",a);
+// console.log("a++ = ",a++);
+// console.log("a = ",a++);
+
+
+
+// console.log("a = ",a);
+// console.log("a-- = ",a--);
+// console.log("a = ",a--);
+
+
+
+// logical operators
+
+
+//lodical AND(&)
+
+// let a = 10;
+// let b = 20;
+
+// let cond1 = a<b; //true
+// let cond2 = a == 10; //true
+// console.log("cond1 && cond2 = ", cond1 && cond2); // True
+
+// let a = 10;
+// let b = 20;
+
+// let cond1 = a>b; //false
+// let cond2 = a == 10; //true
+// console.log("cond1 && cond2 = ", cond1 && cond2); // False
+
+
+ //logical OR(||)
+
+// 1.
+// let a = 10;
+// let b = 20;
+
+// let cond1 = a>b; //false
+// let cond2 = a == 10; //true
+// console.log("cond1 || cond2 = ", cond1 || cond2); // True
+
+
+//2.
+// let a = 10;
+// let b = 20;
+
+// let cond1 = a>b; //false
+// let cond2 = a == 14; //false
+// console.log("cond1 || cond2 = ", cond1 || cond2); // False
+
+
+// logical NOT(!) // (Reverse)
+
+// let a = 10;
+// let b = 20;
+// console.log("!(10 > 20) =", !(a > b));//True
+
+
+
+///Conditional statements
+
+// //1. if statements
+
+// let age = 18;
+
+// if (age >= 18);{
+//     console.log("you can vote");
+// }
+// if (age < 18) {
+//     console.log("You cannot vote");
+// }
+
+
+// let mode = "dark";
+// let colour;
+
+// if (mode === "dark") {
+//     colour = "Black";
+// }
+
+// if (mode === "light") {
+//     colour ="white";
+// }
+// console.log(colour);
+
+// 2. if-else
+
+// let mode = "blue";
+// let colour;
+// if (mode ==="dark") {
+//     colour = "black";
+// } else {
+//     colour ="White";
+// }  
+// console.log(colour)
+
+// let age = 16;
+
+//  if (age >= 18) {
+//       console.log("you can vote");
+//  } else
+//     {console.log("You cannot vote");
+// }
+
+
+// even or odd
+
+let number = "1";
+
+if (number%2 ===0) {
+console.log("even");
+} else {
+    console.log("odd");
+}
+
+
