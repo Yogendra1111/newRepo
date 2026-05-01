727101231dde6e958534dc139a1a89c335542201
yogendra1111 <yogendranagar222005@gmail.com>
2026-05-01 13:39:06 +0530

    loop code

---
+++ loops.js
@@ -0,0 +1,60 @@
+// for loop;
+
+// for(let count = 1; count<=4; count++ ) {
+//     console.log("Helloworld");
+//}
+
+
+
+// Calculate sum of 1 tp 10;
+
+// let sum = 0;
+// for (let i=1; i<=10; i++){
+//     (sum = sum + i);
+// }
+// console.log(sum)
+
+
+
+//while loop;
+
+// let i = 1;
+// while (i <= 10) {
+//     console.log("helloworld");
+//     i++
+// }
+
+
+
+// do while loop;
+
+// let i = 1;
+// do  {
+//     console.log("i = ",i);
+//     i++
+// } while(i <=10);
+
+
+
+// for-of loop;
+
+// let str = "yogendra";
+// let length=0;
+// for(let val of str){
+//     console.log("val = ",val)
+//     length++;
+// }
+// console.log("length of str = ",length);
+
+
+
+//for-in loop;
+let student = {
+    name:"Yogendra",
+    age: 20,
+    cgpa: 9.5,
+    ispass: true,
+};
+for(let key in student) {
+console.log(key,student[key])
+}
