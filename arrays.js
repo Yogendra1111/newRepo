5ca85f6b841fe951d343f4cf65cb83a99fbd7c90
yogendra1111 <yogendranagar222005@gmail.com>
2026-05-04 17:12:14 +0530

    arrays

---
+++ arrays.js
@@ -0,0 +1,23 @@
+
+
+// let array = [ 1,2,3,4,5];
+// str = "str"
+// console.log(array);
+// console.log(array.length);
+
+
+// array indises;
+
+// let arr = [10,11,12,13,14,15,16];
+// console.log(arr[6]);
+// // change the value of aary;
+// arr[6]=10
+// console.log(arr[6]);
+
+
+
+/// looping over an array;
+ let ind = ["RJ",'MH','PNJ','HR','GJ','UK'];
+for (let idx = 0;idx < ind.length ; idx++){
+    console.log(idx,ind[idx]);
+};
