0788bf75e05c9d389a1108d69779f29345363d4a
yogendra1111 <yogendranagar222005@gmail.com>
2026-05-10 16:36:28 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,32 @@
+// function myfunction() {
+//     console.log("We are learning java script");
+//     console.log("in which");
+//     console.log("we are learning functions");
+
+// }
+
+// myfunction();
+
+
+// function mymessage(msg){    // paramiter
+//     console.log(msg);
+// }
+
+// mymessage("we are starting of functions");      //argument
+
+
+// function sum(a,b){
+
+//     console.log(a+b);
+
+// }
+// sum(10,20);
+
+
+// function sum(x,y,z) {
+//     s = x + y + z
+//     return s;
+// }
+
+// let sum_val = sum(67,45,88);
+// console.log(sum_val);
