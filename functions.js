7cc171ac381e55cfc93d0b81b1f8397752d04a0c
yogendra1111 <yogendranagar222005@gmail.com>
2026-05-10 18:24:09 +0530

    finction

--- functions.js
+++ functions.js
@@ -30,3 +30,46 @@
 
 // let sum_val = sum(67,45,88);
 // console.log(sum_val);
+
+
+                                                // Arrow funnction
+
+// (a,b) => {
+//     sum = a + b;
+//     return sum;
+// }
+
+// let summ = (10,20);
+// console.log(summ);
+
+
+// const arrowMul = (a,b) => {
+//     return a * b;
+// }
+
+// arrowMul(2,5);
+
+// const printHello = () => console.log("Hello");
+// console.log(printHello)
+
+
+
+
+                            // practice of functions;
+
+//          code for counting vovels
+
+
+const countVovels = (str) => {
+            let count = 0;
+        for (const char of str) {
+            if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u")
+                // console.log(countVovels("yogendra"));
+            {
+            count++;
+            }
+    }
+    console.log(count);
+}
+countVovels("Yogendra");
+
