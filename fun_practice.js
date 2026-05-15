df37b48fd16f5e9ac75f43d6a70a0d0c2b6118fe
yogendra1111 <yogendranagar222005@gmail.com>
2026-05-15 16:53:15 +0530

    array methods

--- fun_practice.js
+++ fun_practice.js
@@ -51,6 +51,45 @@
 
         // OR
 
-let arr = [1,2,3,4,5];
+// let arr = [1,2,3,4,5];
 
-arr.forEach((val) => {console.log(val);});
+// arr.forEach((val) => {console.log(val);});
+
+
+
+// let arr = [1,2,3,4,5,];
+
+// arr.forEach((num) => {
+// console.log(num*num);
+// });
+
+
+//                                              filter        
+
+// let arr = [1,2,3,4,5,];
+// let highval = arr.filter((num) => {
+//        return num>3
+// });
+// console.log(highval)
+
+//                                              reduce
+
+// let arr = [1,2,3,4];
+// let sumVal = arr.reduce((pre,curr) => {
+//         return pre + curr;
+//  }
+// )
+// console.log(sumVal);
+
+
+//                              reduce method to find the largest number
+
+let arr = [1,2,3,4];
+let largestVal = arr.reduce((pre,curr) => {
+        return pre > curr ? pre : curr;
+ }
+);
+console.log(largestVal);
+
+
+
