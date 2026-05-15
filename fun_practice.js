929430098924d0b2ca66bda21e022237be781829
yogendra1111 <yogendranagar222005@gmail.com>
2026-05-15 14:55:19 +0530

    foreach

--- fun_practice.js
+++ fun_practice.js
@@ -23,18 +23,34 @@
 
                             // practice of functions;
 
-//          code for counting vovels
+//                                          for counting vovels
 
 
-const countVovels = (str) => {
-            let count = 0;
-        for (const char of str) {
-            if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u")
-                // console.log(countVovels("yogendra"));
-            {
-            count++;
-            }
-    }
-    console.log(count);
-}
-countVovels("Yogendra");
+// const countVovels = (str) => {
+//             let count = 0;
+//         for (const char of str) {
+//             if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u")
+                
+//             {
+//             count++;
+//             }
+//     }
+//     console.log(count);
+    
+// }
+// countVovels("Yogendra");
+
+
+
+//                                      foreach             ????????
+// let arr = [1,2,3,4,5];
+
+// arr.forEach(functon printVal(val) {
+//     console.log(val);
+// });
+
+        // OR
+
+let arr = [1,2,3,4,5];
+
+arr.forEach((val) => {console.log(val);});
