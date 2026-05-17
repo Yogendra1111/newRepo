7da35f1a58ac23eb27bff52548e126f33dad23eb
yogendra1111 <yogendranagar222005@gmail.com>
2026-05-17 12:02:46 +0530

    doc

--- js_html_css/html.js
+++ js_html_css/html.js
@@ -1,2 +1,6 @@
 
-alert("Do you want to open this site" );
+// alert("Do you want to open this site" );
+
+let heading = document.getElementById("heading");
+console.log(heading);
+console.dir(heading);
